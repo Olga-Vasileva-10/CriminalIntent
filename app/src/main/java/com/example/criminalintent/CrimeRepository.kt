788b1e0b76deddf70ec281747c698ce6050dea8c
Olga <Vasileva.o.p.2.0.0@gmail.com>
2024-11-21 12:20:48 +0700

    6.2 Создание CrimeDatabase

--- app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -1,38 +1,26 @@
 package com.example.criminalintent
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import com.example.criminalintent.database.CrimeDatabase
+import java.util.UUID
+import java.util.concurrent.Executors
 
 class CrimeRepository private
 constructor(context: Context) {
-    private val database : CrimeDatabase = Room.databaseBuilder(
-        context.applicationContext,
-        CrimeDatabase::class.java,
-        DATABASE_NAME
-    ).build()
-    private val crimeDao = database.crimeDao()
-    private val executor = Executors.newSingleThreadExecutor()
-    fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
-    fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
-
-    fun updateCrime(crime: Crime) {
-        executor.execute {
-            crimeDao.updateCrime(crime)
-        }
-    }
-    fun addCrime(crime: Crime) {
-        executor.execute {
-            crimeDao.addCrime(crime)
-        }
-    }
-
-
     companion object {
-        private var INSTANCE: CrimeRepository? = null
+        private var INSTANCE: CrimeRepository? =
+            null
         fun initialize(context: Context) {
             if (INSTANCE == null) {
-                INSTANCE = CrimeRepository(context)
+                INSTANCE =
+                    CrimeRepository(context)
             }
         }
         fun get(): CrimeRepository {
-            return INSTANCE ?: throw IllegalStateException("CrimeRepository must be initialized")
+            return INSTANCE ?:
+            throw
+            IllegalStateException("CrimeRepository must be initialized")
         }
     }
 }
