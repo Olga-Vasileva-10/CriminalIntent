78ac3bf6cfb231276b7bd4fc065e134eefea2bf6
Olga <Vasileva.o.p.2.0.0@gmail.com>
2024-11-21 12:42:09 +0700

    6.3 Создание диалоговых окон

--- app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val currentFragment =
-            supportFragmentManager.findFragmentById(R.id.fragment_container)
+        val currentFragment =supportFragmentManager.findFragmentById(R.id.fragment_container)
         if (currentFragment == null) {
             val fragment = CrimeListFragment.newInstance()
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
